7a4cc5165ba374f484bf67d0c7825abf4d82fb05
David Y. Stephenson <david@davidystephenson.com>
2026-01-18 14:34:07 -0500

    Upgrade taos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { TAOS } from '@versoly/react-taos'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="bg-red-500 h-96">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="delay-[300ms] duration-[600ms] taos:translate-x-[100%] taos:opacity-0" data-taos-offset="400">test</div>
+      <div className="bg-red-500 h-96"></div>
+      <div className="bg-blue-500 h-96"></div>
+      <div className="bg-yellow-500 h-96"></div>
+      <div className="bg-green-500 h-96"></div>
+      <TAOS
+        className="delay-[300ms] duration-[600ms] taos:translate-y-[100%] taos:opacity-0"
+        data-taos-offset="300"
+      >
+        <h1 className="text-9xl">Test</h1>
+      </TAOS>
+      <div className="bg-purple-500 h-96"></div>
     </>
   )
 }
